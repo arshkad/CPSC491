8a50cf97b114a6b45b4834adbf806817dd8abb30
Herm0x <willism1769@csu.fullerton.edu>
2025-10-13 18:28:05 -0700

    reverted changes

--- server.js
+++ server.js
@@ -28,20 +28,17 @@ app.post('/login', (req, res) => {
 
   fs.readFile('users.txt', 'utf8', (err, data) => {
     if (err) {
-      return res.send('Login failed: Invalid credentials.');
+      return res.redirect('/login%20page.html?error=1');
     }
 
-    const users = data.split('\n');
+    const users = data.split(/\r?\n/);
     const isValidUser = users.some(line => {
       if (!line) return false;
-      // users.txt will be saved as [username, email, password]
       const parts = line.split(':');
-      // Check if username (parts[0]) and password (parts[2]) match
-      return parts[0] === username && parts[2] === password;
+      return parts[0] === username && parts[2] && parts[2].trim() === password;
     });
 
     if (isValidUser) {
-      // If login is successful, send back the script
       res.send(`
         <script>
           sessionStorage.setItem('loggedInUser', '${username}');
@@ -49,7 +46,7 @@ app.post('/login', (req, res) => {
         </script>
       `);
     } else {
-      res.send('<h1>Login Failed</h1><p>Incorrect username or password.</p><a href="/login%20page.html">Try Again</a>');
+      res.redirect('/login%20page.html?error=1');
     }
   });
 });
